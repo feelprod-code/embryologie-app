9cd32f415621491f975fe64ee919463d1984fbb5
Guillaume Philippe <guillaumephilippe1968@gmail.com>
2026-07-17 12:30:22 +0200

    chore(dev): proxy local /api calls to production app

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
         target: 'https://customer-6i2z59dst7q6iswv.cloudflarestream.com',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/cf-stream/, '')
+      },
+      '/api': {
+        target: 'https://app.feelprod.com',
+        changeOrigin: true
       }
     }
   }
