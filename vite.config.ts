d299c7e6c1f4044ff7655b20ab2787d062050ee5
Guillaume Philippe <guillaumephilippe1968@gmail.com>
2026-09-07 15:28:36 +0200

    feat(dev-pwa): dev.html et manifest-dev avec icone web clip dev pour iPhone

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,13 @@ export default defineConfig({
         changeOrigin: true
       }
     }
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        dev: 'dev.html'
+      }
+    }
   }
 })
